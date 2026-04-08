a74168edf8a47f834ac888d13f26deece454c9da
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2026-04-07 21:17:29 -0400

    updated index.JS file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let count = 0
+
+let visitedBtn = document.querySelector("#visited-btn")
+let peopleCounter = document.querySelector("#people-counter")
+let saveBtn = document.querySelector("#save-btn")
+let bottomStripe = document.querySelector(".bottomStripe")
+let resetBtn = document.querySelector("#reset-btn")
+visitedBtn.addEventListener("click", visited)
+saveBtn.addEventListener("click", save)
+resetBtn.addEventListener("click",reset)
+
+
+function visited(){
+    count += 1
+    peopleCounter.textContent = count
+}
+
+function save(){
+    let countSrc = count + " - "
+    bottomStripe.textContent += countSrc 
+
+}
+
+function reset(){
+    bottomStripe.textContent = 0
+    count = 0
+}
